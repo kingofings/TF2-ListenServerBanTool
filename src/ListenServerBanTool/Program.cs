b25ff6792d71826ee1bacc24caf05cce69cab932
kingofings <47538464+kingofings@users.noreply.github.com>
2026-03-04 20:56:56 +0100

    fix self contained build

--- src/ListenServerBanTool/Program.cs
+++ src/ListenServerBanTool/Program.cs
@@ -47,6 +47,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
